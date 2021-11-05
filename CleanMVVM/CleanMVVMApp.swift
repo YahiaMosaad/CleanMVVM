0a329e29bdef27f2c6a014dfc6e1bacdd52a1d70
Yahia Mosaad <yahia.mosaad@gmail.com>
2021-11-05 19:27:28 +0400

    Initial Commit

---
+++ CleanMVVM/CleanMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanMVVMApp.swift
+//  CleanMVVM
+//
+//  Created by Yahia Mosaad on 05/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct CleanMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
